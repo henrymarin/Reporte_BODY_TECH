9f3aed97ad0b2908a04a68025a3fa1c13b17e0b6
HM@rin <hmarin@intergrupo.com>
2018-03-13 06:12:20 -0500

    uso de escel

--- src/main/java/com/bodytech/reporte/controladoras/GenerarReporteController.java
+++ src/main/java/com/bodytech/reporte/controladoras/GenerarReporteController.java
@@ -1,9 +1,15 @@
 package com.bodytech.reporte.controladoras;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.JSONObject;
+import org.jxls.template.SimpleExporter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bodytech.reporte.dtos.BSTReporteResponse;
 import com.bodytech.reporte.dtos.GenericBootStrapTableRequest;
+import com.bodytech.reporte.entidades.Conversacion;
+import com.bodytech.reporte.repositorios.ConversacionRepository;
 import com.bodytech.reporte.servicios.impl.GenerarReporteServiceImpl;
 import com.javainuse.DtoEntrada;
 
@@ -21,6 +29,7 @@ public class GenerarReporteController {
 	
 	@Autowired private GenerarReporteServiceImpl servicio;
 	private static final String APPLICATION_JSON = "application/json";
+	@Autowired private ConversacionRepository conversacionRepository;
 	
 	
 	@RequestMapping(
@@ -46,4 +55,21 @@ public class GenerarReporteController {
 		}
 		return servicio.generarReportePaginado(request);
 	}
+	
+	@RequestMapping(value = "/exportXLS", method = RequestMethod.GET)
+	public void exportXLS(HttpServletResponse response) {
+		List<Conversacion> persons = (List<Conversacion>) conversacionRepository.findAll();
+		List<String> headers = Arrays.asList("ID de la Conversa", "Nombre del Agente");
+		try {
+			response.addHeader("Content-disposition", "attachment; filename=reporteExcel.xls");
+			response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+			
+			new SimpleExporter().gridExport(headers, persons, "idConversacion, nombreAgente, ", response.getOutputStream());
+			response.flushBuffer();
+		} catch (Exception e) {
+
+		}
+	}
+	
+	
 }
